49903e195bbf41a764326a12576ea3c5809fa7dc
macbook <macbook@macbooks-MacBook-Pro.local>
2024-11-04 01:04:54 +0100

    add linked list creation

---
+++ main.c
@@ -0,0 +1,161 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 22:33:12 by macbook           #+#    #+#             */
+/*   Updated: 2024/11/04 01:02:19 by macbook          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_stack_node
+{
+	int					value;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+t_stack_node	*create_node(int value)
+{
+	t_stack_node	*new_node;
+
+	new_node = (t_stack_node *)malloc(sizeof(t_stack_node));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void	add_to_beginning(t_stack_node **head, int value)
+{
+	t_stack_node	*new_node;
+
+	new_node = create_node(value);
+	if (!new_node)
+		return ;
+	if (*head != NULL)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+}
+
+void	add_to_end(t_stack_node **head, int value)
+{
+	t_stack_node	*new_node;
+	t_stack_node	*temp;
+
+	new_node = create_node(value);
+	if (!new_node)
+		return ;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
+}
+
+void	print_list(t_stack_node *head)
+{
+	t_stack_node	*temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		printf("%d --> ", temp->value);
+		temp = temp->next;
+	}
+	printf("NULL\n");
+}
+
+t_stack_node	*remove_from_beginning(t_stack_node **head)
+{
+	t_stack_node	*removed_node;
+
+	if (*head == NULL)
+		return (NULL);
+	removed_node = *head;
+	*head = (*head)->next;
+	if (*head != NULL)
+		(*head)->prev = NULL;
+	removed_node->next = NULL;
+	return (removed_node);
+}
+
+t_stack_node	*remove_from_end(t_stack_node **head)
+{
+	t_stack_node	*temp;
+
+	if (*head == NULL)
+		return (NULL);
+	temp = *head;
+	if (temp->next == NULL)
+	{
+		*head = NULL;
+		temp->prev = NULL;
+		return (temp);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	return (temp);
+}
+
+static void	swap(t_stack_node **stack)
+{
+	t_stack_node	*first_node;
+	t_stack_node	*second_node;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	first_node = *stack;
+	second_node = first_node->next;
+
+	first_node->next = second_node->next;
+	first_node->prev = second_node;
+	
+	if (second_node->next != NULL)
+		second_node->next->prev = first_node;
+
+	second_node->next = first_node;
+	second_node->prev = NULL;
+
+	*stack = second_node;
+}
+
+int	main(void)
+{
+	t_stack_node	*head;
+
+	head = NULL;
+	add_to_beginning(&head, 7);
+	add_to_beginning(&head, -34);
+	add_to_beginning(&head, 1);
+	add_to_beginning(&head, 43);
+	add_to_beginning(&head, 22);
+	add_to_beginning(&head, -98);
+	add_to_beginning(&head, 67);
+	// add_to_end(&head, 30);
+	// add_to_end(&head, 40);
+	print_list(head);
+	swap(&head);
+	print_list(head);
+	return (0);
+}
